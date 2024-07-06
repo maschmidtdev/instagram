4808597d74817321e9ff5499f0ea99c507410fcb
Marcel Schmidt <maschmidt.dev@gmail.com>
2024-07-06 13:55:56 +0200

    Adding new comment functinality to posts and stimulus reset function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,18 @@
 class CommentsController < ApplicationController
-  before_action :set_post, only[:create]
+  before_action :set_post, only: [:create]
 
   def create
     @comment =  @post.comments.create(user: current_user, body: params[:comment_body])
+
+    respond_to do |format|
+      format.turbo_stream do 
+        render turbo_stream: turbo_stream.replace(
+          "post#{@post.id}comments",
+          partial: "posts/post_comments",
+          locals: {post: @post}
+        )
+      end
+    end
   end
 
   def destroy
