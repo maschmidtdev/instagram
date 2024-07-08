e7d1998d5a7d48dad59bdf02124126d1039397f3
Marcel Schmidt <maschmidt.dev@gmail.com>
2024-07-08 11:46:33 +0200

    follow system model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   has_many :posts  
   has_one_attached :profile_picture
 
   has_many :likes
 
   has_many :comments
+
+  has_many :follow_requests, -> where([accepted: false]), class_name: "Follow", foreign_key: "follower_id"
+
+  has_many :accepted_received_requests, -> where([accepted: true]), class_name: "Follow", foreign_key: "followed_id"
+
+  has_many :accepted_sent_requests, -> where([accepted: true]), class_name: "Follow", foreign_key: "follower_id"
+
+  # has_many :received_requests, class_name: "Follow", foreign_key: "followed_id"
+  # has_many :sent_requests, class_name: "Follow", foreign_key: "follower_id"
+  # has_many :waiting_sent_requests, -> where([accepted: false]), class_name: "Follow", foreign_key: "follower_id"
+
+  has_many :followers, throug: :accepted_received_requests, source: :follower
+  has_many :followings, throug: :accepted_sent_requests, source: :followed
+
 end
